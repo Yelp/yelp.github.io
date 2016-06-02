21c97fe92dc2bf22f6b3ba4b261194604bab98d8
Alex Levy <alex.levy@alumni.tufts.edu>
2016-06-01 22:39:31 -0700

    Fail a bit more gracefully when GitHub throttles you

--- js/data/_reload.sh
+++ js/data/_reload.sh
@@ -3,18 +3,33 @@
 cd $(dirname $0)
 
 # repos.json
-echo -n > repos.json
-response=
-let page=1
-while [ "$response" != "[]" ]
-do
-  url="https://api.github.com/users/yelp/repos?per_page=100&page=$page"
-  echo $url
-  response=$(curl "$url" | jq . | tee -a repos.json.tmp)
-  let page=page+1
-done
-cat repos.json.tmp | jq '.[]' | jq -s . > repos.json
-rm repos.json.tmp
+
+get_repos() {
+  local outfile=$(mktemp)
+  local response=
+  local page=1
+
+  while [ "$response" != "[]" ]
+  do
+    url="https://api.github.com/users/yelp/repos?per_page=100&page=$page"
+    echo "$url" >&2
+
+    response=$(curl -sf "$url" | jq . | tee -a "$outfile")
+
+    if [ $? -eq 0 ]
+    then
+      echo "curl failed, so I'm gonna get out of here." >&2
+      exit 1
+    fi
+
+    let page=page+1
+  done
+
+  cat "$outfile" | jq '.[]' | jq -s .
+}
+
+get_repos || echo '[]' > repos.json
 
 # members.json
-curl "https://api.github.com/orgs/yelp/members" > members.json
+
+curl -sf "https://api.github.com/orgs/yelp/members" || echo '[]' > members.json
