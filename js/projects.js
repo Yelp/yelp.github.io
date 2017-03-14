3de24fd3ea4b45dea6ffbf9e62615cf6fe23a7c2
Ben Plotnick <plotnick@yelp.com>
2017-03-14 11:50:43 -0700

    http -> https

--- js/projects.js
+++ js/projects.js
@@ -1,54 +1,54 @@
 oss_projects = {};
 oss_projects['dockersh'] = {
-  blog_post: 'http://engineeringblog.yelp.com/2014/08/hack209-dockersh.html',
+  blog_post: 'https://engineeringblog.yelp.com/2014/08/hack209-dockersh.html',
   featured: true,
   position: 4,
   background: 'dockersh_bg.png'
 }
 oss_projects['osxcollector'] = {
-  blog_post: 'http://engineeringblog.yelp.com/2015/01/osxcollector-forensic-collection-and-automated-analysis-for-os-x.html',
+  blog_post: 'https://engineeringblog.yelp.com/2015/01/osxcollector-forensic-collection-and-automated-analysis-for-os-x.html',
   featured: true,
   position: 3,
   background: 'osxcollector_bg.png'
 };
 oss_projects['paasta'] = {
-  blog_post: 'http://engineeringblog.yelp.com/2015/11/introducing-paasta-an-open-platform-as-a-service.html',
+  blog_post: 'https://engineeringblog.yelp.com/2015/11/introducing-paasta-an-open-platform-as-a-service.html',
   featured: true,
   position: 1,
   background: 'paasta_bg.png'
 };
 oss_projects['elastalert'] = {
-  blog_post: 'http://engineeringblog.yelp.com/2015/10/elastalert-alerting-at-scale-with-elasticsearch.html',
+  blog_post: 'https://engineeringblog.yelp.com/2015/10/elastalert-alerting-at-scale-with-elasticsearch.html',
   featured: true,
   position: 2,
   background: 'elastalert.png'
 }
 oss_projects['mrjob'] = {
-  blog_post: 'http://engineeringblog.yelp.com/2010/10/mrjob-distributed-computing-for-everybody.html'
+  blog_post: 'https://engineeringblog.yelp.com/2010/10/mrjob-distributed-computing-for-everybody.html'
 }
 oss_projects['yelp-ruby'] = {
-  blog_post: 'http://engineeringblog.yelp.com/2014/04/more-yelp-in-your-ruby.html'
+  blog_post: 'https://engineeringblog.yelp.com/2014/04/more-yelp-in-your-ruby.html'
 };
 oss_projects['firefly'] = {
-  blog_post: 'http://engineeringblog.yelp.com/2012/08/firefly-illuminate-your-websites-performance.html',
+  blog_post: 'https://engineeringblog.yelp.com/2012/08/firefly-illuminate-your-websites-performance.html',
 };
 oss_projects['pyleus'] = {
-  blog_post: 'http://engineeringblog.yelp.com/2014/10/introducing-pyleus.html',
+  blog_post: 'https://engineeringblog.yelp.com/2014/10/introducing-pyleus.html',
 };
 oss_projects['MOE'] = {
-  blog_post: 'http://engineeringblog.yelp.com/2014/07/introducing-moe-metric-optimization-engine-a-new-open-source-machine-learning-service-for-optimal-ex.html'
+  blog_post: 'https://engineeringblog.yelp.com/2014/07/introducing-moe-metric-optimization-engine-a-new-open-source-machine-learning-service-for-optimal-ex.html'
 }
 oss_projects['fullerite'] = {
-  blog_post: 'http://engineeringblog.yelp.com/2015/10/fullerite-a-new-mineral-to-collect-metrics.html'
+  blog_post: 'https://engineeringblog.yelp.com/2015/10/fullerite-a-new-mineral-to-collect-metrics.html'
 }
 oss_projects['mycroft'] = {
-  blog_post: 'http://engineeringblog.yelp.com/2015/04/mycroft-load-data-into-redshift-automatically.html'
+  blog_post: 'https://engineeringblog.yelp.com/2015/04/mycroft-load-data-into-redshift-automatically.html'
 }
 oss_projects['YLTableView'] = {
-  blog_post: 'http://engineeringblog.yelp.com/2015/06/advanced-uitableviews-made-simple-yltableview.html'
+  blog_post: 'https://engineeringblog.yelp.com/2015/06/advanced-uitableviews-made-simple-yltableview.html'
 }
 oss_projects['amira'] = {
-  blog_post: 'http://engineeringblog.yelp.com/2016/08/amira-automated-malware-incident-response-and-analysis.html'
+  blog_post: 'https://engineeringblog.yelp.com/2016/08/amira-automated-malware-incident-response-and-analysis.html'
 }
 oss_projects['dumb-init'] = {
   blog_post: 'https://engineeringblog.yelp.com/2016/01/dumb-init-an-init-for-docker.html'
